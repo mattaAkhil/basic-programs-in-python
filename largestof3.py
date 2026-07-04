005e46ad176a3aec7cfce6161c3fa0a463b0d198
akhil_matta23 <148411952+Hfdnjdssj@users.noreply.github.com>
2026-07-04 11:37:48 +0530

    basic programs

---
+++ largestof3.py
@@ -0,0 +1,12 @@
+def large(a,b,c):
+    if a>b and a>c:
+        return a
+    elif b>a and b>c:
+        return b
+    else:
+        return c 
+n1=int(input("Enter first number: "))
+n2=int(input("Enter second number: "))
+n3=int(input("Enter third number: "))
+largest=large(n1,n2,n3)
+print("The largest number is:",largest)       
---
+++ quo_rem.py
@@ -0,0 +1,6 @@
+n=int(input("Enter a number: "))
+divisor=int(input("enter a divisor:"))
+quotient=n//divisor
+remainder=n%divisor
+print("The quotient is:", quotient)
+print("The remainder is:", remainder)
